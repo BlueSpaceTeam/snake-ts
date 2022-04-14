f87e8fdf18c4df29338a60e61885bde123a5ce14
FantiGA <98534535+FantiGA@users.noreply.github.com>
2022-04-14 22:45:49 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 /*
  * @Author: fantiga
  * @Date: 2022-04-14 22:01:46
- * @LastEditTime: 2022-04-14 22:39:15
+ * @LastEditTime: 2022-04-14 22:44:11
  * @LastEditors: fantiga
  * @Description: 
  * @FilePath: /snake-ts/src/index.ts
  */
+
+const sum = (a: number, b: number): number => a + b
+
+console.log(sum(123, 456))
 
 const str: string = 'Hello world!'
 
