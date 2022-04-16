c8e2092288c4fc27835bc1bdab48684ca034c645
swancai <734665222@qq.com>
2022-04-16 12:31:21 +0800

    fix: fix class getter and setter

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,12 @@
 import './scss/base.scss'
 import './scss/snake.scss'
 
-
 import { ScoreBoard } from './components/ScoreBoard'
 
+
+// 调用展示-TODEL
 const scoreBoardObj = new ScoreBoard()
-
-// 调用
 setTimeout(() => {
-    scoreBoardObj.addScore(100)
-
+    scoreBoardObj.addScore(11)
     console.log(scoreBoardObj.rank)
 }, 5000)
