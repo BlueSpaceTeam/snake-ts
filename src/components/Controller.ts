40e37ba8487a6d1ca6945c20f765ea758ff8c1d7
FantiGA <98534535+FantiGA@users.noreply.github.com>
2022-04-16 16:07:16 +0800

    Update Controller.ts
    + 添加键盘响应事件，修改方向

--- src/components/Controller.ts
+++ src/components/Controller.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: fantiga
  * @Date: 2022-04-16 15:15:45
- * @LastEditTime: 2022-04-16 15:30:43
+ * @LastEditTime: 2022-04-16 16:05:32
  * @LastEditors: fantiga
  * @Description: 
  * @FilePath: /snake-ts/src/components/Controller.ts
@@ -16,6 +16,9 @@ class Controller {
     snake: Snake
     scoreBoard: ScoreBoard
 
+    // 移动方向（keydown的值）
+    direction: string = ''
+
     constructor() {
         this.food = new Food()
         this.snake = new Snake()
@@ -27,13 +30,19 @@ class Controller {
 
     // 游戏初始化方法，调用即开始游戏
     init() {
-        // 绑定键盘按下事件
-        document.addEventListener('keydown', this.keyboardHandler)
+        /**
+         * 绑定键盘按下事件
+         * this.keyboardHandler中的this，是document对象
+         * 如果需要给Controller本身绑定，则一定要加bind，bind实际上是创建了一个新的函数
+         */
+        document.addEventListener('keydown', this.keyboardHandler.bind(this))
     }
 
     // 键盘事件响应
     keyboardHandler = (event: KeyboardEvent) => {
-        console.log(event.key)
+        console.log(this)
+        // 修改方向
+        this.direction = event.key
     }
 }
 
