e0c3d98771e4e64f20cf393c044b62bb2f4c591e
swancai <734665222@qq.com>
2022-04-16 20:09:40 +0800

    update the scoreboard attrs

--- src/components/ScoreBoard.ts
+++ src/components/ScoreBoard.ts
@@ -4,19 +4,29 @@
  * 每 2分 升一个等级，(对应属性deltaScore)
  * */
 
+/**
+ * 计分板
+ * @param speed：速度，默认每2分一个等级
+ * @param maxLevel：最大等级，默认是100
+ * 
+ * 对象属性（可赋值及取值）：
+ * totalScore: 当前总分
+ * level: 当前等级
+ * 
+ **/ 
 export default class ScoreBoard {
 	private _totalScore: number = 0 // 总分
 	private _level: number = 0 // 等级
+	private _speed: number // 速度。等级level = 函数f(分数score, 速度speed)
+	private _maxLevel: number // 最大等级
 
 	elScore: HTMLElement = document.getElementById('score')! // 总分元素Dom对象
 	elLevel: HTMLElement = document.getElementById('level')!// 等级元素Dom对象
 
-	speed: number // 速度。等级level = 函数f(分数score, 速度speed)
-	maxLevel: number // 最大等级
 
 	constructor (speed: number = 2, maxLevel: number = 100 ) {
-		this.maxLevel = maxLevel 
-		this.speed = speed
+		this._maxLevel = maxLevel 
+		this._speed = speed
 	}
 
 	get totalScore(): number { // 获取总分
@@ -38,10 +48,10 @@ export default class ScoreBoard {
 	}
 
 	private handleLevelMap(totalScore: number = 0): void { // 处理等级规则
-		const { speed, maxLevel } = this
+		const { _speed, _maxLevel } = this
 
-		for (let i: number = 1; i <= maxLevel; i++) {
-			if (totalScore <= i * speed) { // 在当前范围内，映射成字符
+		for (let i: number = 1; i <= _maxLevel; i++) {
+			if (totalScore <= i * _speed) { // 在当前范围内，映射成字符
 				this.level = i
 				break
 			}
