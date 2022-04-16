4aacc23728ce2703ba11e222b3d222a61426ea19
FantiGA <98534535+FantiGA@users.noreply.github.com>
2022-04-16 15:38:52 +0800

    * 优化ScoreBoard部分
    不需要as断言，因为不会有null发生的情况，所以只需要加!即可

--- src/components/ScoreBoard.ts
+++ src/components/ScoreBoard.ts
@@ -8,31 +8,31 @@ export default class ScoreBoard {
 	private _totalScore: number = 0 // 总分
 	private _rank: string = 'A' // 等级
 
-	elScore: HTMLElement = document.getElementById('score') as HTMLElement // 总分元素Dom对象
-	elRank: HTMLElement = document.getElementById('rank') as HTMLElement// 等级元素Dom对象
+	elScore: HTMLElement = document.getElementById('score')! // 总分元素Dom对象
+	elRank: HTMLElement = document.getElementById('rank')!// 等级元素Dom对象
 
 	deltaScore: number = 2 // 等差数列中的公差
 
-	get totalScore (): number { // 获取总分
+	get totalScore(): number { // 获取总分
 		return this._totalScore
 	}
-	set totalScore (newVal: number) { // 设置总分
+	set totalScore(newVal: number) { // 设置总分
 		this._totalScore = newVal
 		this.elScore.innerText = newVal + ''
 		this.handleRankMap(newVal)
 	}
 
-	get rank (): string { // 获取等级
+	get rank(): string { // 获取等级
 		// return this.elRank.innerText
 		return this._rank
 	}
 
-	set rank (newVal) { // 设置等级
+	set rank(newVal) { // 设置等级
 		this.elRank.innerText = newVal
 		this._rank = newVal
 	}
 
-	private handleRankMap (totalScore: number = 0): void { // 处理等级规则
+	private handleRankMap(totalScore: number = 0): void { // 处理等级规则
 		const { deltaScore } = this
 		for (let i: number = 0; i <= 26; i++) {
 			if (totalScore <= (i + 1) * deltaScore) { // 在当前范围内，映射成字符
