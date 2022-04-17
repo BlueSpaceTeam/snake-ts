9dfe4bfdc4231307e68e1b322ff259eae0dbe91f
swancai <734665222@qq.com>
2022-04-17 22:15:16 +0800

    add modal

--- src/index.ts
+++ src/index.ts
@@ -23,23 +23,7 @@ modal.showModal({
     title: '你好',
     content: 'hahah',
     success: (b = '') => {
-        if (b === 'replay') {
-            setTimeout(() => {
-                modal.showModal({
-                    title: '你好11111111',
-                    content: 'hahah11111111111111111',
-                    subContent: 'sssssssss',
-                    success: (b = '') => {
-                        if (b === 'replay') {
-                            
-                        } else {}
-                    },
-                    fail: (err: any) => {
-                        console.error(err)
-                    }
-                })
-            }, 5000)
-        } else {}
+        if (b === 'replay') {} else {}
     },
     fail: (err: any) => {
         console.error(err)
